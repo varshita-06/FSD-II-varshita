21985396665c0251e05deec8086e99c1ef4ba027
varshita-06 <varshitadevibolla@gmail.com>
2026-08-12 13:45:30 +0530

    Delete week7/server.js

--- week7/server.js
+++
@@ -1,8 +0,0 @@
-const app = require('./dynamic');
-
-require('./Accept_from_inputs');
-require('./validation');
-
-app.listen(3000, () => {
-    console.log('Server running at http://localhost:3000');
-});
